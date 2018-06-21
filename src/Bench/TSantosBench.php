2536692da41c2e72b6d9b250312a2dc1242fa4ee
tsantos <tales.augusto.santos@gmail.com>
2018-06-21 01:12:33 -0300

    Updated tsantos serializer

--- src/Bench/TSantosBench.php
+++ src/Bench/TSantosBench.php
@@ -2,14 +2,46 @@
 
 namespace TSantos\Benchmark\Bench;
 
+use Metadata\Driver\FileLocator;
+use Symfony\Component\Filesystem\Filesystem;
+use TSantos\Benchmark\AbstractBench;
 use TSantos\Benchmark\Person;
+use TSantos\Serializer\Metadata\Driver\YamlDriver;
+use TSantos\Serializer\SerializerBuilder;
+use TSantos\Serializer\SerializerInterface;
 
 /**
- * Class TSantosAccessorBench
- * @package TSantos\Benchmark\Bench
+ * Class TSantosBench
+ *
+ * @author Tales Santos <tales.augusto.santos@gmail.com>
  */
-class TSantosBench extends AbstractTSantosBench
+class TSantosBench extends AbstractBench
 {
+    /**
+     * @var SerializerInterface
+     */
+    protected $serializer;
+
+    /**
+     * @var Filesystem
+     */
+    protected $fs;
+
+    public function bootstrap(): void
+    {
+        $fileLocator = new FileLocator(['TSantos\Benchmark' => $this->getResourceDir('/mappings/tsantos')]);
+
+        $this->createCacheDir('/hydrators', '/metadata');
+
+        $this->serializer = (new SerializerBuilder())
+            ->setMetadataDriver(new YamlDriver($fileLocator))
+            ->setHydratorDir($this->getCacheDir('/hydrators'))
+            ->setMetadataCacheDir($this->getCacheDir('/metadata'))
+            ->enableBuiltInNormalizers()
+            ->setDebug(false)
+            ->build();
+    }
+
     protected function doBenchSerialize(array $objects): void
     {
         $this->serializer->serialize($objects);
@@ -22,6 +54,11 @@ class TSantosBench extends AbstractTSantosBench
 
     public function getName(): string
     {
-        return 'TSantos (accessors)';
+        return 'TSantos';
+    }
+
+    public function getPackageName(): string
+    {
+        return 'tsantos/serializer';
     }
 }
